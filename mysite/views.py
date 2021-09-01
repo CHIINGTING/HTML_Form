6a2efc55cbd18fe24771a02516c9db06775f465f
CHIINGTING <tad20077@gmail.com>
2021-09-01 23:42:58 +0800

    create html

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def get_example(request):
+    try:
+        urid = request.GET['user_id']
+        urpass = request.GET['user_pass']
+    except:
+        urid = None
+
+    years = range(1960, 2021 + 1)
+    return render(request, 'get_example.html', locals())
