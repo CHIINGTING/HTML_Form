a7ef459fa501d8484d1d7cb90cd334bdd1df318c
CHIINGTING <tad20077@gmail.com>
2021-09-08 15:32:26 +0800

    add form

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from mysite import models
+from mysite import forms
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = forms.ContactForm(request.POST)
+        if form.is_valid():
+            message = "感謝您的來信。"
+            user_name = form.cleaned_data['user_name']
+            user_city = form.cleaned_data['user_city']
+            user_school = form.cleaned_data['user_school']
+            user_email = form.cleaned_data['user_email']
+            user_message = form.cleaned_data['user_message']
+        else:
+            message = "請檢查您輸入的資訊是否正確！"
+    else:
+        form = forms.ContactForm()
+    return render(request, 'contact.html', locals())
 
 
 # Create your views here.
@@ -51,3 +69,43 @@ def index(request, pid=None, del_pass=None):
             message = "密碼錯誤"
 
     return render(request, 'index.html', locals())
+
+
+def listing(request):
+    posts = models.Post.objects.filter(enabled=True).order_by('-pub_time')[:150]
+    moods = models.Mood.objects.all()
+    return render(request, 'listing.html', locals())
+
+
+def posting(request):
+    moods = models.Mood.objects.all()
+    message = '如要張貼訊息，則每一個欄位都要填...'
+    try:
+        user_id = request.POST['user_id']
+        user_pass = request.POST['user_pass']
+        user_post = request.POST['user_post']
+        user_mood = request.POST['mood']
+    except:
+        user_id = None
+        message = '張貼每一則訊息,每個欄位都要填'
+    if user_id is not None:
+        mood = models.Mood.objects.get(status=user_mood)
+        post = models.Post.objects.create(mood=mood, nickname=user_id, del_pass=user_pass, message=user_post)
+        post.save()
+        message = '儲存成功!記得編輯你的密碼[{}],之後才會顯示'.format(user_pass)
+    return render(request, 'posting.html', locals())
+
+
+def post2db(request):
+    if request.method == 'POST':
+        post_form = forms.PostForm(request.POST)
+        if post_form.is_valid():
+            message = "您的訊息已儲存，要等管理者啟用後才看得到喔。"
+            post_form.save()
+            return HttpResponseRedirect('/list/')
+        else:
+            message = '如要張貼訊息，則每一個欄位都要填...'
+    else:
+        post_form = forms.PostForm()
+        message = '如要張貼訊息，則每一個欄位都要填... '
+    return render(request, 'post2db.html', locals())
